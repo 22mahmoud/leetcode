e3b419901aad3f6c75d8fbd9cfaed35daf8f0a04
22mahmoud <hello@mahmoudashraf.dev>
2024-06-03 14:49:16 +0300

    refactor

--- problems/167_two_sum_ii_input_array_is_sorted/index.ts
+++ problems/167_two_sum_ii_input_array_is_sorted/index.ts
@@ -1,18 +1,18 @@
 function twoSum(numbers: number[], target: number): number[] {
-  let left = 0;
-  let right = numbers.length - 1;
+  let l = 0;
+  let r = numbers.length - 1;
 
-  while (true) {
-    const sum = numbers[left] + numbers[right];
-    if (sum > target) {
-      right--;
-    } else if (sum < target) {
-      left++;
-    } else {
-      return [left + 1, right + 1];
-    }
+  while (l < r) {
+    const sum = numbers[l] + numbers[r];
+
+    if (sum === target) return [l + 1, r + 1];
+
+    if (sum > target) r--;
+
+    if (sum < target) l++;
   }
+
+  throw new Error("No Two Sum solution");
 }
 
 export { twoSum };
-
