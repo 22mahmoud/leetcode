ac50647fec98a325c9d5831b2b0045f4befcf0b0
22mahmoud <hello@mahmoudashraf.dev>
2024-06-11 14:18:42 +0300

    17 fix test

--- problems/17_letter_combinations_of_a_phone_number/index.test.ts
+++ problems/17_letter_combinations_of_a_phone_number/index.test.ts
@@ -1,12 +1,43 @@
-import { checkSubarraySum } from './index.js';
+import { letterCombinations } from './index.js';
 
 describe.each([
-  { k: 13, nums: [23, 2, 6, 4, 7], expected: false },
-  { k: 6, nums: [23, 2, 6, 4, 7], expected: true },
-  { k: 6, nums: [23, 2, 4, 6, 7], expected: true },
-  { k: 7, nums: [23, 2, 4, 6, 6], expected: true },
-])('describe function checkSubarraySum($nums, $k)', ({ nums, k, expected }) => {
+  { digits: '23', expected: ['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf'] },
+  { digits: '', expected: [] },
+  { digits: '2', expected: ['a', 'b', 'c'] },
+  {
+    digits: '234',
+    expected: [
+      'adg',
+      'adh',
+      'adi',
+      'aeg',
+      'aeh',
+      'aei',
+      'afg',
+      'afh',
+      'afi',
+      'bdg',
+      'bdh',
+      'bdi',
+      'beg',
+      'beh',
+      'bei',
+      'bfg',
+      'bfh',
+      'bfi',
+      'cdg',
+      'cdh',
+      'cdi',
+      'ceg',
+      'ceh',
+      'cei',
+      'cfg',
+      'cfh',
+      'cfi',
+    ],
+  },
+])('describe function letterCombinations($digits)', ({ digits, expected }) => {
   test(`Returns ${expected}`, () => {
-    expect(checkSubarraySum(nums, k)).toBe(expected);
+    expect(letterCombinations(digits)).toStrictEqual(expected);
   });
 });
